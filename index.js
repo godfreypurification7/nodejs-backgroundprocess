e4ff1bc137b10e0d11d927418bba1ee821a33a40
Godfrey Purification <godfreypurification71@gmail.com>
2026-03-06 18:06:08 +0600

    Remove comments from index.js
    Removed header comments from index.js

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-/*
- * Title: Uptime Monitoring Application
- * Description: A RESTFul API to monitor up or down time of user defined links
- * Author: Sumit Saha ( Learn with Sumit )
- * Date: 11/15/2020
- *
- */
+
 // dependencies
 const workers=require('./lib/worker');
 const server=require('./lib/server');
